1a9dcced61e98b6db2359c2bd3a5264b9d5c40b8
DoJun Park <dojun527@naver.com>
2021-03-21 14:10:02 +0900

    update 1932

--- 백준/1932.py
+++ 백준/1932.py
@@ -1,10 +1,20 @@
-n = int(input())
-a = [list(map(int,input().split())) for _ in range(n)]
-d = [[0]*n for i in range(n)]
-d[0][0] = a[0][0]
-for i in range(1, n):
+N = int(input())
+A = [list(map(int, input().split())) for _ in range(N)]
+mem = [[0]*N for i in range(N)]
+
+mem[0][0] = A[0][0]
+
+for i in range (1, N):
     for j in range(0, i+1):
-        d[i][j] = d[i-1][j] + a[i][j]
-        if j-1 >= 0 and d[i][j] < d[i-1][j-1] + a[i][j]:
-            d[i][j] = d[i-1][j-1] + a[i][j]
-print(max(d[n-1]))
+        if i == 0:
+            mem[i][j] = mem[i-1][j] + A[i][j]
+
+        mem[i][j] = max(mem[i-1][j], mem[i-1][j-1]) + A[i][j]
+        # if j>0 and mem[i][j] < mem[i-1][j-1] + A[i][j]:    
+        #     mem[i][j] = mem[i-1][j-1] + A[i][j]
+
+
+print(max(mem[N-1]))
+
+
+
