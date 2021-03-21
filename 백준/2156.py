01d9e264465a04cb9500625cf06e62c4bfc1a9a8
DoJun Park <dojun527@naver.com>
2021-03-21 11:25:13 +0900

    update 2156

--- 백준/2156.py
+++ 백준/2156.py
@@ -1,14 +1,12 @@
-n = int(input())
-a = [0] + [int(input()) for _ in range(n)]
-d = [0] * (n+1)
+N = int(input())
+A = [0] + list(int(input()) for _ in range(N))
+mem = [0] * (N+1)
+mem[1] = A[1]
+if(N>1):
+    mem[2] = A[1] + A[2]
 
-d[1] = a[1]
-if n>= 2:
-    d[2] = a[1] + a[2]
 
-for i in range(3, n+1):
-    d[i] = d[i-1]
-    d[i] = max(d[i], d[i-2]+a[i])
-    d[i] = max(d[i], d[i-3]+a[i]+a[i-1])
+for i in range(3,N+1):
+    mem[i] = max(mem[i-1], mem[i-2]+A[i], mem[i-3]+A[i-1]+A[i])
 
-print(d[n])
+print(mem[N])
